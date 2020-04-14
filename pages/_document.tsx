289a40dfd299f62db909bc5106c5393af083caea
Ofek Ashery <ofek.ashery@gmail.com>
2020-04-14 17:29:47 +0300

    Add Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,18 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-43849303-8" />
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-43849303-8');
+              `
+            }}
+          />
         </body>
       </Html>
     );
