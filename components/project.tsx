ba7048defcc5958021bf0c07c1b47028edb21d69
Ofek Ashery <ofek.ashery@gmail.com>
2021-05-03 00:04:47 +0300

    Update Next.js version

--- components/project.tsx
+++ components/project.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Project = ({ title, titleFormatted = title, description, link, logo }) => {
   return (
     <>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 
@@ -8,11 +8,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#000" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
-          rel="preload"
-          as="style"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
         <link rel="preconnect" href="https://www.google-analytics.com" />
         <link rel="preconnect" href="https://www.googletagmanager.com" />
       </Head>
