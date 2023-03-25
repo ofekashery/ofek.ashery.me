1b2243120703cbb7954096f9205460bc53eae338
Ofek Ashery <ofek.ashery@gmail.com>
2023-03-25 13:16:37 +0300

    Use latest metadata API

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,33 @@
-import { Inter } from '@next/font/google';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
 });
+
+export const metadata: Metadata = {
+  title: 'Ofek Ashery',
+  description: 'A 19-year-old passionate developer from Israel.',
+  themeColor: '#101010',
+  icons: {
+    shortcut: '/assets/favicon.png',
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://ofek.ashery.me',
+    title: 'Ofek Ashery',
+    description: 'A 19-year-old passionate developer from Israel.',
+    images: [
+      {
+        url: 'https://ofek.ashery.me/assets/og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
